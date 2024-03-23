c80792f93bb5ffd4b897216e99bce0e7faf06a28
Michael Koenig <koenigmichael2002@gmail.com>
2024-03-23 19:30:21 +0100

    Randomize mobs

--- feat/main/main.gd
+++ feat/main/main.gd
@@ -4,54 +4,57 @@ extends Node
 var score
 
 func _ready():
-	pass
+    pass
 
 func game_over():
-	$ScoreTimer.stop()
-	$MobTimer.stop()
-	$HUD.show_game_over()
+    $ScoreTimer.stop()
+    $MobTimer.stop()
+    $HUD.show_game_over()
 
 func new_game():
-	# remove all old mobs (from previous round)
-	get_tree().call_group("mobs", "queue_free")
+    # remove all old mobs (from previous round)
+    get_tree().call_group("mobs", "queue_free")
 
-	score = 0
-	$Player.start($StartPosition.position)
-	$StartTimer.start()
-	$HUD.update_score(score)
-	$HUD.show_message("Get Ready")
+    score = 0
+    $Player.start($StartPosition.position)
+    $StartTimer.start()
+    $HUD.update_score(score)
+    $HUD.show_message("Get Ready")
 
 func _on_score_timer_timeout():
-	score += 1
-	$HUD.update_score(score)
+    score += 1
+    $HUD.update_score(score)
 
 func _on_start_timer_timeout():
-	$MobTimer.start()
-	$ScoreTimer.start()
+    $MobTimer.start()
+    $ScoreTimer.start()
 
 func _on_mob_timer_timeout():
-	var mob = mob_scene.instantiate()
+    var mob = mob_scene.instantiate()
+    
+    var types = mob.get_node("AnimatedSprite2D").sprite_frames.get_animation_names()
+    mob.get_node("AnimatedSprite2D").play(types[randi() % types.size()])
 
-	# Choose a random location on Path2D.
-	var mob_spawn_location = $MobPath/MobSpawnLocation
-	mob_spawn_location.progress_ratio = randf()
+    # Choose a random location on Path2D.
+    var mob_spawn_location = $MobPath/MobSpawnLocation
+    mob_spawn_location.progress_ratio = randf()
 
-	# Set the mob's direction perpendicular to the path direction.
-	var direction = mob_spawn_location.rotation + PI / 2
+    # Set the mob's direction perpendicular to the path direction.
+    var direction = mob_spawn_location.rotation + PI / 2
 
-	# Set the mob's position to a random location.
-	mob.position = mob_spawn_location.position
+    # Set the mob's position to a random location.
+    mob.position = mob_spawn_location.position
 
-	# Add some randomness to the direction.
-	direction += randf_range( - PI / 4, PI / 4)
+    # Add some randomness to the direction.
+    direction += randf_range( - PI / 4, PI / 4)
 
-	#if (direction < 0):
-		#mob.flip_h()
-		#print("moving right?")
+    #if (direction < 0):
+        #mob.flip_h()
+        #print("moving right?")
 
-	# Choose the velocity for the mob.
-	#var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
-	#mob.linear_velocity = velocity.rotated(direction)
+    # Choose the velocity for the mob.
+    #var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+    #mob.linear_velocity = velocity.rotated(direction)
 
-	# Spawn the mob by adding it to the Main scene.
-	add_child(mob)
+    # Spawn the mob by adding it to the Main scene.
+    add_child(mob)
