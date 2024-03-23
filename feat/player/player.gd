bf3e99748df6760d7190720932b8087f4f1dfe5b
Michael Koenig <koenigmichael2002@gmail.com>
2024-03-23 16:46:46 +0100

    Fix dying

--- feat/player/player.gd
+++ feat/player/player.gd
@@ -7,60 +7,58 @@ var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	hide()
-	screen_size = get_viewport_rect().size
+    hide()
+    screen_size = get_viewport_rect().size
 
 func _get_velocity():
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+    var velocity = Vector2.ZERO
+    if Input.is_action_pressed("move_right"):
+        velocity.x += 1
+    if Input.is_action_pressed("move_left"):
+        velocity.x -= 1
+    if Input.is_action_pressed("move_down"):
+        velocity.y += 1
+    if Input.is_action_pressed("move_up"):
+        velocity.y -= 1
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-	
-	return velocity
+    if velocity.length() > 0:
+        velocity = velocity.normalized() * speed
+    
+    return velocity
 
 func _process(delta):
-	var velocity = _get_velocity()
-	var cursor_pos = get_global_mouse_position()
-	var player_pos = global_position
+    var velocity = _get_velocity()
+    var cursor_pos = get_global_mouse_position()
+    var player_pos = global_position
 
-	if velocity.length() > 0:
-		$PlayerSprite.play()
-	else:
-		$PlayerSprite.stop()
+    if velocity.length() > 0:
+        $PlayerSprite.play()
+    else:
+        $PlayerSprite.stop()
 
-	if velocity.x != 0:
-		$PlayerSprite.animation = "walk"
+    if velocity.x != 0:
+        $PlayerSprite.animation = "walk"
 
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+    position += velocity * delta
+    position = position.clamp(Vector2.ZERO, screen_size)
 
-	# Flip sprites
-	if cursor_pos.x > player_pos.x:
-		scale.x = -1
-	else:
-		scale.x = 1
+    # Flip sprites
+    if cursor_pos.x > player_pos.x:
+        scale.x = -1
+    else:
+        scale.x = 1
 
-	$GunSprite.look_at(cursor_pos)
-	$GunSprite.flip_h = true
+    $GunSprite.look_at(cursor_pos)
+    $GunSprite.flip_h = true
 
 func _on_body_entered(body: Node2D):
-	hide() # Player disappears after being hit.
-	hit.emit()
-	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred("disabled", true)
+    print("player hit")
+    hide() # Player disappears after being hit.
+    hit.emit()
+    # Must be deferred as we can't change physics properties on a physics callback.
+    $CollisionShape2D.set_deferred("disabled", true)
 
 func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
-
-
-
+    position = pos
+    show()
+    $CollisionShape2D.disabled = false
